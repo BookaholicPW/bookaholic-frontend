d827a0e85f4dc966d3688e77bf17b9069a0ef4a4
Minh Nguyen Cong <mcong@box.com>
2023-04-15 23:30:17 +0200

    Update endpoints & schemas

---
+++ src/configs/endpoints.tsx
@@ -0,0 +1,215 @@
+import { ApiResponseBodyBase, Book, BookCharacter, BookGenre, UserBase, UserToken } from "./schemas";
+
+/**
+ * @description
+ * This file contains all the endpoints of the API.
+ */
+
+export namespace AccountRegister {
+    /**
+     * @description
+     * This endpoint is used to register a new user.
+     */
+
+    export const path = '/account/register';
+    export const method = 'POST';
+    export type RequestBody = {
+        email: string;
+        password: string;
+    }
+    export type ResponseBody = ApiResponseBodyBase & {
+        data?: UserBase
+    }
+}
+
+export namespace AccountLogin {
+    /**
+     * @description
+     * This endpoint is used to login a user.
+     * It returns user information with a JWT token that can be used to authenticate the user.
+     */
+
+    export const path = '/account/login';
+    export const method = 'POST';
+    export type RequestBody = {
+        email: string;
+        password: string;
+    }
+    export type ResponseBody = ApiResponseBodyBase & {
+        data?: UserBase & {
+            token: UserToken;
+        }
+    }
+}
+
+export namespace AccountUpdate {
+    /**
+     * @description
+     * This endpoint is used to update current user's account information.
+     * If any of the fields is not provided, it will not be updated.
+     * if any fields set to null, it will be removed (empty).
+     */
+    export const path = '/account';
+    export const method = 'PUT';
+    export type RequestBody = {
+        username?: string;
+        bio?: string;
+        avatar?: string;
+        favoriteBooks?: Book[];
+        favoriteAuthors?: string[];
+        favoriteGenres?: string[];
+    }
+    export type ResponseBody = ApiResponseBodyBase & {
+        data?: UserBase
+    }
+}
+
+export namespace AccountGet {
+    /**
+     * @description
+     * This endpoint is used to get current user's account information.
+     */
+    export const path = '/account';
+    export const method = 'GET';
+    export type RequestBody = undefined;
+    export type ResponseBody = ApiResponseBodyBase & {
+        data?: UserBase
+    }
+}
+
+export namespace AccountLogout {
+    /**
+     * @description
+     * This endpoint is used to logout current user.
+     * It will invalidate the JWT token.
+     */
+    export const path = '/account/logout';
+    export const method = 'POST';
+    export type RequestBody = undefined;
+    export type ResponseBody = ApiResponseBodyBase & {
+        data?: undefined
+    }
+}
+
+export namespace ListBooks {
+    /**
+     * @description
+     * This endpoint is used to get a list of books.
+     */
+    export const path = '/books';
+    export const method = 'GET';
+    export type RequestBody = undefined;
+    export type ResponseBody = ApiResponseBodyBase & {
+        data?: Book[]
+    }
+}
+
+export namespace GetBook {
+    /**
+     * @description
+     * This endpoint is used to get a book by its id.
+     */
+    export const path = '/books/:id';
+    export const method = 'GET';
+    export type RequestBody = undefined;
+    export type ResponseBody = ApiResponseBodyBase & {
+        data?: Book
+    }
+}
+
+export namespace FindBooks {
+    /**
+     * @description
+     * This endpoint is used to find books by their title.
+     */
+    export const path = '/books/find';
+    export const method = 'POST';
+    export type RequestBody = {
+        term: string;
+    };
+    export type ResponseBody = ApiResponseBodyBase & {
+        data?: Book[]
+    }
+}
+
+export namespace ListBookGenres {
+    /**
+     * @description
+     * This endpoint is used to get a list of book genres.
+     * Without return books that belong to each genre.
+     */
+    export const path = '/book-genres';
+    export const method = 'GET';
+    export type RequestBody = undefined;
+    export type ResponseBody = ApiResponseBodyBase & {
+        data?: BookGenre[]
+    }
+}
+
+export namespace GetBookGenre {
+    /**
+     * @description
+     * This endpoint is used to get a book genre by its id.
+     * It will also return a list of books that belong to this genre.
+     */
+    export const path = '/book-genres/:id';
+    export const method = 'GET';
+    export type RequestBody = undefined;
+    export type ResponseBody = ApiResponseBodyBase & {
+        data?: BookGenre
+    }
+}
+
+export namespace ListBookCharacters {
+    /**
+     * @description
+     * This endpoint is used to get a list of book characters.
+     */
+    export const path = '/book-characters';
+    export const method = 'GET';
+    export type RequestBody = undefined;
+    export type ResponseBody = ApiResponseBodyBase & {
+        data?: BookCharacter[]
+    }
+}
+
+export namespace GetBookCharacter {
+    /**
+     * @description
+     * This endpoint is used to get a book character by its id.
+     * It will also return a list of books that this character belongs to.
+     */
+    export const path = '/book-characters/:id';
+    export const method = 'GET';
+    export type RequestBody = undefined;
+    export type ResponseBody = ApiResponseBodyBase & {
+        data?: BookCharacter
+    }
+}
+
+export namespace ListBookAuthor {
+    /**
+     * @description
+     * This endpoint is used to get a list of book authors.
+     */
+    export const path = '/book-authors';
+    export const method = 'GET';
+    export type RequestBody = undefined;
+    export type ResponseBody = ApiResponseBodyBase & {
+        data?: string[]
+    }
+}
+
+export namespace GetBookAuthor {
+    /**
+     * @description
+     * This endpoint is used to get a book author by its id.
+     * It will also return a list of books that this author wrote.
+     */
+    export const path = '/book-authors/:id';
+    export const method = 'GET';
+    export type RequestBody = undefined;
+    export type ResponseBody = ApiResponseBodyBase & {
+        data?: string
+    }
+}
---
+++ src/configs/schemas.tsx
@@ -0,0 +1,94 @@
+// Description: This file contains all the schemas used in the app
+// Path: src/configs/schemas.tsx
+
+export const appConfig: {
+    appName: string;
+    siteName: string;
+    siteUrl: string;
+    siteDescription: string;
+    siteKeywords: string;
+    apiRoot: string;
+} = {
+    appName: 'bookaholic',
+    siteName: 'Bookaholic',
+    siteUrl: 'https://bookaholic.pl',
+    siteDescription: 'Bookaholic - connect all book lovers',
+    siteKeywords: 'bookaholic, book, books, book lovers, book lovers community, bookaholic community, bookaholic.pl, bookaholic.pl community',
+    apiRoot: 'https://api.bookaholic.pl',
+}
+
+export type ApiRequestBodyBase = any;
+
+export type ApiResponseBodyBase = {
+    status: 'success' | 'error';
+    message: string;
+    data: any;
+}
+
+export type ApiEndpoint = {
+    url: string;
+    method: 'GET' | 'POST' | 'PUT' | 'DELETE';
+    headers?: {
+        [key: string]: string;
+    };
+    requestBody?: any;
+    responseBody?: any;
+}
+
+export type Author = {
+    id: string; // uuid v4
+    name: string;
+    characters?: BookCharacter[];
+    avatar?: string;
+    born?: number;
+    died?: number;
+    books?: Book[];
+    nationality?: string;
+    originalLanguage?: string;
+}
+
+
+export type Book = {
+    id: string; // uuid v4
+    title: string;
+    description?: string;
+    authors?: Author[];
+    genres?: BookGenre[];
+    cover?: string;
+    pages?: number;
+    published?: number;
+}
+
+export type BookGenre = {
+    id: string; // uuid v4
+    name: string;
+    books?: Book[];
+}
+
+export type BookCharacter = {
+    id: string; // uuid v4
+    name: string;
+    books?: Book[];
+}
+
+export type UserBase = {
+    id: string; // uuid v4
+    username: string;
+    email: string;
+    avatar?: string;
+    bio?: string;
+    favoriteBooks?: Book[];
+    favoriteAuthors?: Author[];
+    favoriteGenres?: BookGenre[];
+}
+
+export type User = UserBase & {
+    password: string;
+    createdAt: number;
+    updatedAt: number;
+}
+
+export type UserToken = {
+    accessToken: string;
+    refreshToken: string;
+}
