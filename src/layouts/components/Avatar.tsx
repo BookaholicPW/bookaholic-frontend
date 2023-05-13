c9e8b29bda973238fbfad1829a1d20f472430ae2
Minh Nguyen Cong <clovers1254@gmail.com>
2023-05-13 03:49:21 +0200

    Basic main layout (#13)

---
+++ src/layouts/components/Avatar.tsx
@@ -0,0 +1,19 @@
+
+
+export default function Avatar(props: {
+    alt: string
+    src: string
+    width: string
+    height: string
+}) {
+    const { src, width, height } = props
+    return (
+        <img src={src} width={width} height={height} style={{
+            borderRadius: '50%',
+            display: 'block',
+            marginLeft: 'auto',
+            marginRight: '0',
+        }}>
+        </img>
+    )
+}
